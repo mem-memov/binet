7e335aa5de120c4bcd853cadfd6f7083b1acc51a
Alexander Novikov <mem-memov@yandex.ru>
2022-08-29 00:49:49 +0300

    refs

--- src/main/scala/net/mem_memov/binet/hexagon/Dot.scala
+++ src/main/scala/net/mem_memov/binet/hexagon/Dot.scala
@@ -4,7 +4,7 @@ import zio.*
 import net.mem_memov.binet.memory
 
 class Dot(
-  private val inventory: Inventory,
+  private val inventory: UIO[Ref[Inventory]],
   val address: memory.Address,
   private val entry: Entry
 ):
@@ -31,29 +31,37 @@ class Dot(
     for {
       incrementedAddress <- entry.address3.increment
       newEntry = entry.copy(address3 = incrementedAddress)
-      updatedInventory <- inventory.update(address, newEntry)
-    } yield new Dot(updatedInventory, address, newEntry)
+      _ <- inventory.modify { inventory =>
+        ((), inventory.update(address, newEntry))
+      }
+    } yield new Dot(inventory, address, newEntry)
 
   def decrementSourceCount: Task[Dot] =
     for {
       decrementedAddress <- entry.address3.decrement
       newEntry = entry.copy(address3 = decrementedAddress)
-      updatedInventory <- inventory.update(address, newEntry)
-    } yield new Dot(updatedInventory,address, newEntry)
+      _ <- inventory.modify { inventory =>
+        ((), inventory.update(address, newEntry))
+      }
+    } yield new Dot(inventory,address, newEntry)
 
   def incrementTargetCount: Task[Dot] =
     for {
       incrementedAddress <- entry.address4.increment
       newEntry = entry.copy(address4 = incrementedAddress)
-      updatedInventory <- inventory.update(address, newEntry)
-    } yield new Dot(updatedInventory, address, newEntry)
+      _ <- inventory.modify { inventory =>
+        ((), inventory.update(address, newEntry))
+      }
+    } yield new Dot(inventory, address, newEntry)
 
   def decrementTargetCount: Task[Dot] =
     for {
       decrementedAddress <- entry.address4.decrement
       newEntry = entry.copy(address4 = decrementedAddress)
-      updatedInventory <- inventory.update(address, newEntry)
-    } yield new Dot(updatedInventory,address, newEntry)
+      _ <- inventory.modify { inventory =>
+        ((), inventory.update(address, newEntry))
+      }
+    } yield new Dot(inventory,address, newEntry)
 
   def sourceArrow: Option[Arrow] =
     Option.unless(entry.address5.isZero) {
