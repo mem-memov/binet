ce879f369bcb9feb1058b5e7995c06538fda3b0a
Alexander Novikov <anovikov@tsum.ru>
2022-08-31 10:59:10 +0300

    WIP

--- src/main/scala/net/mem_memov/binet/hexagon/connecting/SourceDotEmpty.scala
+++ src/main/scala/net/mem_memov/binet/hexagon/connecting/SourceDotEmpty.scala
@@ -2,4 +2,4 @@ package net.mem_memov.binet.hexagon.connecting
 
 import net.mem_memov.binet.hexagon.{Arrow, Connecting}
 
-class TargetDotNonEmpty(targetDotSourceArrow: Arrow) extends Connecting
+class SourceDotEmpty(targetDotSourceArrow: Arrow) extends Connecting
--- src/main/scala/net/mem_memov/binet/hexagon/connecting/TargetDotEmpty.scala
+++ src/main/scala/net/mem_memov/binet/hexagon/connecting/TargetDotEmpty.scala
@@ -3,6 +3,6 @@ package net.mem_memov.binet.hexagon.connecting
 import net.mem_memov.binet.hexagon.{Arrow, Connecting, Connection}
 import zio.*
 
-class SourceDotNonEmpty(sourceDotTargetArrow: Arrow) extends Connecting:
+class TargetDotEmpty(sourceDotTargetArrow: Arrow) extends Connecting:
 
   def connect: Task[Connection] = ???
