990af42be8cedec6cdffeb3d696d410fd0780d8b
Alexander Novikov <anovikov@tsum.ru>
2022-10-13 11:27:12 +0300

    level tests

--- src/test/scala/net/mem_memov/binet/memory/LevelSuite.scala
+++ src/test/scala/net/mem_memov/binet/memory/LevelSuite.scala
@@ -5,7 +5,15 @@ class LevelSuite extends munit.FunSuite:
   test("Level creates stores for keeping addresses") {
     val level = Level.top
     val store = level.createStore
-    assert(store.isInstanceOf[Store])
+
+    val destination = UnsignedByte.fromInt(7)
+    val content = Address.apply(List(UnsignedByte.fromInt(32)))
+    store.write(destination, content) match {
+      case Left(error) => fail(error)
+      case Right(updatedStore) =>
+        val result = updatedStore.read(destination)
+        assert(result == content)
+    }
   }
 
   test("Level creates stocks for connection to child elements") {
