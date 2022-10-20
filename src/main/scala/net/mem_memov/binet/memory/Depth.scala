d8042dabe62e96dad3443c1b56a28d65aec2768e
Alexander Novikov <anovikov@tsum.ru>
2022-10-20 18:22:44 +0300

    docs

--- src/main/scala/net/mem_memov/binet/memory/Depth.scala
+++ src/main/scala/net/mem_memov/binet/memory/Depth.scala
@@ -1,5 +1,9 @@
 package net.mem_memov.binet.memory
 
+/**
+ * Depth indicates the level a store was written at.
+ * It facilitates store expansion.
+ */
 trait Depth:
   
   def expandStore(store: Store): Store
