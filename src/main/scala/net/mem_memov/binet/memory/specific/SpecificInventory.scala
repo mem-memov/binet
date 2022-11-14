63ea51c96897b22fc87ca0f4066d8d77c744c989
Alexander Novikov <anovikov@tsum.ru>
2022-11-14 17:10:52 +0300

    inventory, tests passing

--- src/main/scala/net/mem_memov/binet/memory/specific/SpecificInventory.scala
+++ src/main/scala/net/mem_memov/binet/memory/specific/SpecificInventory.scala
@@ -1,10 +1,10 @@
 package net.mem_memov.binet.memory.specific
 
-import net.mem_memov.binet.memory.general.inventory.InventoryAppender
+import net.mem_memov.binet.memory.general.inventory.{InventoryAppender, InventoryNextAddressProvider, InventoryReader, InventoryUpdater}
+import net.mem_memov.binet.memory.general.element.{ElementReader, ElementWriter}
+import net.mem_memov.binet.memory.general.address.{AddressIncrementer, AddressToContentConverter, AddressToPathConverter, AddressTrimmer}
 import net.mem_memov.binet.memory.specific.specificInventory.specific.SpecificArgument
 import net.mem_memov.binet.memory.specific.specificInventory.general.argument.{CheckAndTrimPermissive, CheckAndTrimRestrictive}
-import net.mem_memov.binet.memory.general.element.ElementWriter
-import net.mem_memov.binet.memory.general.address.{AddressToPathConverter, AddressToContentConverter, AddressIncrementer}
 
 case class SpecificInventory(
   next: SpecificAddress,
@@ -12,6 +12,15 @@ case class SpecificInventory(
 )
 
 object SpecificInventory:
+
+  given InventoryNextAddressProvider[SpecificInventory, SpecificAddress] with
+
+    override
+    def nextInInventory(
+      inventory: SpecificInventory
+    ): SpecificAddress =
+
+      inventory.next
 
   given [ARGUMENT](using
     argument: ARGUMENT
@@ -34,3 +43,44 @@ object SpecificInventory:
         updatedRoot <- inventory.root.write(inventory.next.toPath, trimmedContent.toContent)
         newNext <- Right(inventory.next.increment)
       } yield inventory.copy(next = newNext, root = updatedRoot)
+
+  given [ARGUMENT](using
+    argument: ARGUMENT
+  )(using
+    CheckAndTrimRestrictive[ARGUMENT, SpecificAddress],
+    ElementWriter[SpecificElement, SpecificPath, SpecificContent],
+    AddressToPathConverter[SpecificAddress, SpecificPath],
+    AddressToContentConverter[SpecificAddress, SpecificContent]
+  ): InventoryUpdater[SpecificInventory, SpecificAddress] with
+
+    override def updateInventory(
+      inventory: SpecificInventory,
+      destination: SpecificAddress,
+      content: SpecificAddress
+    ): Either[String, SpecificInventory] =
+
+      for {
+        trimmedDestination <- argument.checkAndTrimRestrictive(inventory.next, destination)
+        trimmedContent <- argument.checkAndTrimRestrictive(inventory.next, content)
+        updatedRoot <- inventory.root.write(trimmedDestination.toPath, trimmedContent.toContent)
+      } yield inventory.copy(root = updatedRoot)
+
+  given [ARGUMENT](using
+    argument: ARGUMENT
+  )(using
+    CheckAndTrimRestrictive[ARGUMENT, SpecificAddress],
+    ElementReader[SpecificElement, SpecificPath, SpecificContent],
+    AddressToPathConverter[SpecificAddress, SpecificPath],
+    AddressTrimmer[SpecificAddress]
+  ): InventoryReader[SpecificInventory, SpecificAddress] with
+
+    override
+    def readInventory(
+      inventory: SpecificInventory,
+      origin: SpecificAddress
+    ): Either[String, SpecificAddress] =
+
+      for {
+        trimmedOrigin <- argument.checkAndTrimRestrictive(inventory.next, origin)
+        content <- inventory.root.read(trimmedOrigin.toPath)
+      } yield content.toAddress.trimBig
