edd40d8531bcf6cd51f182fcd32c7752f7e93488
Alexander Novikov <anovikov@tsum.ru>
2022-11-22 13:29:20 +0300

    checker tests

--- src/test/scala/net/mem_memov/binet/memory/inventory/argument/checker/CheckBoundaryPermissivelySuite.scala
+++ src/test/scala/net/mem_memov/binet/memory/inventory/argument/checker/CheckBoundaryPermissivelySuite.scala
@@ -1,7 +1,73 @@
 package net.mem_memov.binet.memory.inventory.argument.checker
 
+import net.mem_memov.binet.memory.{general, specific}
+import net.mem_memov.binet.memory.specific.inventory.specific.argument.specific.Checker
+import net.mem_memov.binet.memory.specific.inventory.specific.argument.specific.Checker.given
+
 class CheckBoundaryPermissivelySuite extends munit.FunSuite:
 
-  test("") {
+  class AddressStub
+  val inventoryNextAddressStub: AddressStub = new AddressStub
+  val checkedAddressStub: AddressStub = new AddressStub
+  val zeroAddressStub: AddressStub = new AddressStub
 
+  class FactoryStub
+  given factoryStub: FactoryStub = new FactoryStub
+
+  test("Checker approves an address below the inventory boundary") {
+
+    given Ordering[AddressStub] with
+      override def compare(x: AddressStub, y: AddressStub): Int =
+        (x, y) match
+          case (inventoryNextAddressStub, checkedAddressStub) => 1
+          case _ => fail("unexpected")
+
+    given general.factory.ZeroAddress[FactoryStub, AddressStub] with
+      override def f(): AddressStub = fail("unexpected")
+
+    val checker = new Checker
+
+    val result = checker.checkBoundaryPermissively(inventoryNextAddressStub, checkedAddressStub)
+
+    assert(result.isRight)
+  }
+
+  test("Checker approves the first address at the inventory boundary") {
+
+    given Ordering[AddressStub] with
+      override def compare(x: AddressStub, y: AddressStub): Int =
+        (x, y) match
+          case (inventoryNextAddressStub, checkedAddressStub) => 1
+          case (inventoryNextAddressStub, zeroAddressStub) => 0
+          case _ => fail("unexpected")
+
+    given general.factory.ZeroAddress[FactoryStub, AddressStub] with
+      override def f(): AddressStub =
+        zeroAddressStub
+
+    val checker = new Checker
+
+    val result = checker.checkBoundaryPermissively(inventoryNextAddressStub, checkedAddressStub)
+
+    assert(result.isRight)
+  }
+
+  test("Checker disapproves an address above the inventory boundary") {
+
+    given Ordering[AddressStub] with
+      override def compare(x: AddressStub, y: AddressStub): Int =
+        (x, y) match
+          case (inventoryNextAddressStub, checkedAddressStub) => -1
+          case (inventoryNextAddressStub, zeroAddressStub) => 1
+          case _ => fail("unexpected")
+
+    given general.factory.ZeroAddress[FactoryStub, AddressStub] with
+      override def f(): AddressStub =
+        zeroAddressStub
+
+    val checker = new Checker
+
+    val result = checker.checkBoundaryPermissively(inventoryNextAddressStub, checkedAddressStub)
+
+    assert(result == Left("Address out of permissive boundary"))
   }
--- src/test/scala/net/mem_memov/binet/memory/inventory/argument/checker/CheckBoundaryRestrictivelySuite.scala
+++ src/test/scala/net/mem_memov/binet/memory/inventory/argument/checker/CheckBoundaryRestrictivelySuite.scala
@@ -1,7 +1,56 @@
 package net.mem_memov.binet.memory.inventory.argument.checker
 
+import net.mem_memov.binet.memory.{general, specific}
+import net.mem_memov.binet.memory.specific.inventory.specific.argument.specific.Checker
+import net.mem_memov.binet.memory.specific.inventory.specific.argument.specific.Checker.given
+
 class CheckBoundaryRestrictivelySuite extends munit.FunSuite:
 
-  test("") {
+  class AddressStub
+  val inventoryNextAddressStub: AddressStub = new AddressStub
+  val checkedAddressStub: AddressStub = new AddressStub
 
+  test("Checker approves an address below the inventory boundary") {
+
+    given Ordering[AddressStub] with
+      override def compare(x: AddressStub, y: AddressStub): Int =
+        (x, y) match
+          case (inventoryNextAddressStub, checkedAddressStub) => 1
+          case _ => fail("unexpected")
+
+    val checker = new Checker
+
+    val result = checker.checkBoundaryRestrictively(inventoryNextAddressStub, checkedAddressStub)
+
+    assert(result.isRight)
+  }
+
+  test("Checker disapproves an address above the inventory boundary") {
+
+    given Ordering[AddressStub] with
+      override def compare(x: AddressStub, y: AddressStub): Int =
+        (x, y) match
+          case (inventoryNextAddressStub, checkedAddressStub) => -1
+          case _ => fail("unexpected")
+
+    val checker = new Checker
+
+    val result = checker.checkBoundaryRestrictively(inventoryNextAddressStub, checkedAddressStub)
+
+    assert(result == Left("Address out of restrictive boundary"))
+  }
+
+  test("Checker disapproves an address at the inventory boundary") {
+
+    given Ordering[AddressStub] with
+      override def compare(x: AddressStub, y: AddressStub): Int =
+        (x, y) match
+          case (inventoryNextAddressStub, checkedAddressStub) => 0
+          case _ => fail("unexpected")
+
+    val checker = new Checker
+
+    val result = checker.checkBoundaryRestrictively(inventoryNextAddressStub, checkedAddressStub)
+
+    assert(result == Left("Address out of restrictive boundary"))
   }
