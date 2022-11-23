1ada367fe5354c9b31ec8b316052e556f0fa4739
Alexander Novikov <anovikov@tsum.ru>
2022-11-23 15:33:30 +0300

    inventory tests

--- src/test/scala/net/mem_memov/binet/memory/inventory/FoldSuite.scala
+++ src/test/scala/net/mem_memov/binet/memory/inventory/FoldSuite.scala
@@ -1,7 +1,41 @@
 package net.mem_memov.binet.memory.inventory
 
+import net.mem_memov.binet.memory.{general, specific}
+import net.mem_memov.binet.memory.specific.{Address, Element, Inventory}
+import net.mem_memov.binet.memory.specific.Inventory.given
+
 class FoldSuite extends munit.FunSuite:
 
-  test("") {
+  class FactoryStub
+  given FactoryStub = new FactoryStub
 
+  class WalkerStub
+  given WalkerStub = new WalkerStub
+
+  test("Inventory folds to a value") {
+
+    val inventoryNextAddress = Address(List.empty)
+    val inventoryRootElement = Element(None, None)
+
+    val startAddress = Address(List.empty)
+    val contentAddress = Address(List.empty)
+
+    val processingFunction = (accumulator: Int, item: general.Item[Address]) => accumulator + 1
+
+    given general.factory.ZeroAddress[FactoryStub, Address] with
+      override def f(): Address =
+        startAddress
+
+    given specific.inventory.general.walker.Travel[WalkerStub, Address] with
+      override def f[RESULT](walker: WalkerStub, result: RESULT, origin: Address, process: (RESULT, general.Item[Address]) => RESULT): Either[String, RESULT] =
+        assert(result == 0)
+        assert(origin.equals(startAddress))
+        assert(process.equals(processingFunction))
+        Right(process(result, general.Item(origin, contentAddress)))
+
+    val inventory = new Inventory(inventoryNextAddress, inventoryRootElement)
+
+    for {
+      result <- inventory.fold[Int](0)(processingFunction)
+    } yield assert(result == 1)
   }
--- src/test/scala/net/mem_memov/binet/memory/inventory/ReadSuite.scala
+++ src/test/scala/net/mem_memov/binet/memory/inventory/ReadSuite.scala
@@ -1,7 +1,62 @@
 package net.mem_memov.binet.memory.inventory
 
+import net.mem_memov.binet.memory.{general, specific}
+import net.mem_memov.binet.memory.specific.{Address, Element, Inventory}
+import net.mem_memov.binet.memory.specific.Inventory.given
+
 class ReadSuite extends munit.FunSuite:
 
-  test("") {
+  class ArgumentStub
+  given ArgumentStub = new ArgumentStub
 
+  class ContentStub
+  class PathStub
+
+  test("Inventory reads content at path") {
+
+    val inventoryNextAddress = Address(List.empty)
+    val inventoryRootElement = Element(None, None)
+
+    val originAddress = Address(List.empty)
+    val trimmedOriginAddress = Address(List.empty)
+
+    val contentStub = new ContentStub
+    val pathStub = new PathStub
+
+    val contentAddress = Address(List.empty)
+    val trimmedContentAddress = Address(List.empty)
+
+    given specific.inventory.general.argument.CheckAndTrimRestrictive[ArgumentStub, Address] with
+      override def f(argument: ArgumentStub, next: Address, address: Address): Either[String, Address] =
+        assert(next.equals(inventoryNextAddress))
+        if address.equals(originAddress) then
+          Right(trimmedOriginAddress)
+        else
+          fail("unexpected")
+
+    given general.address.ToPath[Address, PathStub] with
+      override def f(address: Address): PathStub =
+        assert(address.equals(trimmedOriginAddress))
+        pathStub
+
+    given general.element.Read[Element, PathStub, ContentStub] with
+      override def f(element: Element, origin: PathStub): Either[String, ContentStub] =
+        assert(origin.equals(pathStub))
+        Right(contentStub)
+
+    given general.content.ToAddress[ContentStub, Address] with
+      override def f(content: ContentStub): Address =
+        assert(content.equals(contentStub))
+        contentAddress
+
+    given general.address.TrimBig[Address] with
+      override def f(address: Address): Address =
+        assert(address.equals(contentAddress))
+        trimmedContentAddress
+
+    val inventory = new Inventory(inventoryNextAddress, inventoryRootElement)
+
+    for {
+      result <- inventory.read(originAddress)
+    } yield assert(result.equals(trimmedContentAddress))
   }
