eb4363da81229c74099cf8688746beba687cea99
Alexander Novikov <anovikov@tsum.ru>
2022-11-23 13:48:15 +0300

    inventory tests

--- src/test/scala/net/mem_memov/binet/memory/inventory/AppendSuite.scala
+++ src/test/scala/net/mem_memov/binet/memory/inventory/AppendSuite.scala
@@ -1,7 +1,64 @@
 package net.mem_memov.binet.memory.inventory
 
+import net.mem_memov.binet.memory.{general, specific}
+import net.mem_memov.binet.memory.specific.{Address, Element, Inventory}
+import net.mem_memov.binet.memory.specific.Inventory.given
+
 class AppendSuite extends munit.FunSuite:
+
+  class ArgumentStub
+  given ArgumentStub = new ArgumentStub
+
+  class ContentStub
+  class PathStub
   
   test("") {
-    
+
+    val originalInventoryNextAddress = Address(List.empty)
+    val incrementedInventoryNextAddress = Address(List.empty)
+
+    val originalInventoryRootElement = Element(None, None)
+    val updatedInventoryRootElement = Element(None, None)
+
+    val contentAddress = Address(List.empty)
+    val trimmedContentAddress = Address(List.empty)
+
+    val contentStub = new ContentStub
+    val inventoryNextPathStub = new PathStub
+
+    given specific.inventory.general.argument.CheckAndTrimPermissive[ArgumentStub, Address] with
+      override def f(argument: ArgumentStub, next: Address, address: Address): Either[String, Address] =
+        assert(next.equals(originalInventoryNextAddress))
+        assert(address.equals(contentAddress))
+        Right(trimmedContentAddress)
+
+    given general.address.ToContent[Address, ContentStub] with
+      override def f(address: Address): ContentStub =
+        assert(address.equals(contentAddress))
+        contentStub
+
+    given general.address.ToPath[Address, PathStub] with
+      override def f(address: Address): PathStub =
+        assert(address.equals(originalInventoryNextAddress))
+        inventoryNextPathStub
+
+    given general.element.Write[Element, PathStub, ContentStub] with
+      override def f(element: Element, destination: PathStub, content: ContentStub): Either[String, Element] =
+        assert(element.equals(originalInventoryRootElement))
+        assert(destination.equals(inventoryNextPathStub))
+        assert(content.equals(contentStub))
+        Right(updatedInventoryRootElement)
+
+    given general.address.Increment[Address] with
+      override def f(address: Address): Address =
+        assert(address.equals(originalInventoryNextAddress))
+        incrementedInventoryNextAddress
+
+    val inventory = new Inventory(originalInventoryNextAddress, originalInventoryRootElement)
+
+    for {
+      result <- inventory.append(contentAddress)
+    } yield
+      assert(result.next.equals(incrementedInventoryNextAddress))
+      assert(result.root.equals(updatedInventoryRootElement))
   }
--- src/test/scala/net/mem_memov/binet/memory/inventory/UpdateSuite.scala
+++ src/test/scala/net/mem_memov/binet/memory/inventory/UpdateSuite.scala
@@ -1,7 +1,64 @@
 package net.mem_memov.binet.memory.inventory
 
+import net.mem_memov.binet.memory.{general, specific}
+import net.mem_memov.binet.memory.specific.{Address, Element, Inventory}
+import net.mem_memov.binet.memory.specific.Inventory.given
+
 class UpdateSuite extends munit.FunSuite:
 
-  test("") {
+  class ArgumentStub
+  given ArgumentStub = new ArgumentStub
 
+  class ContentStub
+  class PathStub
+
+  test("Inventory updates path with content") {
+
+    val originalInventoryNextAddress = Address(List.empty)
+
+    val originalInventoryRootElement = Element(None, None)
+    val updatedInventoryRootElement = Element(None, None)
+
+    val contentAddress = Address(List.empty)
+    val trimmedContentAddress = Address(List.empty)
+
+    val pathAddress = Address(List.empty)
+    val trimmedPathAddress = Address(List.empty)
+
+    val contentStub = new ContentStub
+    val pathStub = new PathStub
+
+    given specific.inventory.general.argument.CheckAndTrimRestrictive[ArgumentStub, Address] with
+      override def f(argument: ArgumentStub, next: Address, address: Address): Either[String, Address] =
+        assert(next.equals(originalInventoryNextAddress))
+        if address.equals(contentAddress) then
+          Right(trimmedContentAddress)
+        else if address.equals(pathAddress) then
+          Right(trimmedPathAddress)
+        else
+          fail("unexpected")
+
+    given general.address.ToContent[Address, ContentStub] with
+      override def f(address: Address): ContentStub =
+        assert(address.equals(trimmedContentAddress))
+        contentStub
+
+    given general.address.ToPath[Address, PathStub] with
+      override def f(address: Address): PathStub =
+        assert(address.equals(trimmedPathAddress))
+        pathStub
+
+    given general.element.Write[Element, PathStub, ContentStub] with
+      override def f(element: Element, destination: PathStub, content: ContentStub): Either[String, Element] =
+        assert(destination.equals(pathStub))
+        assert(content.equals(contentStub))
+        Right(updatedInventoryRootElement)
+
+    val inventory = new Inventory(originalInventoryNextAddress, originalInventoryRootElement)
+
+    for {
+      result <- inventory.update(pathAddress, contentAddress)
+    } yield
+      assert(result.next.equals(originalInventoryNextAddress))
+      assert(result.root.equals(updatedInventoryRootElement))
   }
